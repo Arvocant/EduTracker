d13ced967661101e028bab7a472a7423dab18aaf
Arvo cant <aro.vant@student.ap.be>
2022-03-28 14:33:30 +0200

    Added Stf

--- edutracker/lib/Students/studentList.dart
+++ edutracker/lib/Students/studentList.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class StudentList extends StatefulWidget {
+  const StudentList({Key? key}) : super(key: key);
+
+  @override
+  State<StudentList> createState() => _StudentListState();
+}
+
+class _StudentListState extends State<StudentList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
